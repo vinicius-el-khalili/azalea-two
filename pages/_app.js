c51b822efcea9f1c3eaf8c1a7d2855d98d13a8b2
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-04-10 19:50:55 -0300

    add AppContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import '@/styles/globals.css'
 
+import { useState } from 'react'
+import AppContext from '@/context/AppContext'
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  const [x,setX] = useState(0)
+  const [y,setY] = useState("hihi")
+
+
+  return (
+  <>
+
+  <AppContext.Provider
+    value={{
+      state:{x,y},
+      setX,setY
+    }}>
+
+    <Component {...pageProps} />
+
+  </AppContext.Provider> 
+
+  </>
+  )
 }
