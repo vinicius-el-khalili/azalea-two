72cdbdd9f0ca01fa8484c3c57238a18b44bb3b55
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-04-12 18:48:34 -0300

    @Cube: cleaner code

--- components/cube/cube.js
+++ components/cube/cube.js
@@ -11,14 +11,15 @@ const Cube = () => {
 
     <div className={style.scene}>        
         <div className={`${style.cube} ${context.layoutStyleMap.cubeRotationMap[context.state.page]}`}>
-            <CubeFace page={context.layoutStyleMap.pages[0]}>
-                <p>Vinicius El Khalili</p>
-                <p>Front end developer</p></CubeFace>
-            <CubeFace page={context.layoutStyleMap.pages[1]}>{context.layoutStyleMap.pages[1]}</CubeFace>
-            <CubeFace page={context.layoutStyleMap.pages[2]}>{context.layoutStyleMap.pages[2]}</CubeFace>
-            <CubeFace page={context.layoutStyleMap.pages[3]}>{context.layoutStyleMap.pages[3]}</CubeFace>
-            <CubeFace page={context.layoutStyleMap.pages[4]}>{context.layoutStyleMap.pages[4]}</CubeFace>
-            <CubeFace page={context.layoutStyleMap.pages[5]}>{context.layoutStyleMap.pages[5]}</CubeFace>
+            {context.layoutStyleMap.pages.map( page => (
+                <CubeFace page={page} key={"cubeface_"+page}>
+                    {
+                        page=="Home"?
+                        (<><p>Vinicius El Khalili</p><p>Front end developer</p></>):
+                        page
+                    }
+                </CubeFace>
+            ))}
             
         </div>
     </div>
