1f34c53ca47245f23c7dc1bece1cdc0f6add2523
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-04-12 00:18:24 -0300

    @navButton: context-dependent font color

--- components/navButton/navButton.js
+++ components/navButton/navButton.js
@@ -4,17 +4,25 @@ import AppContext from "@/context/AppContext"
 import Link from "next/link"
 
 const NavButton = ({title,page,tag}) => {
-
     const context = useContext(AppContext)
     const handleClick = () => {
         context.setPage(tag)
     }
-
+    const fontColorMap = {
+        "Home":         "red",
+        "Projects":     "blue",
+        "Blog":         "green",
+        "CV":           "black",
+        "Technologies": "black",
+        "Contact":      "black",
+    }
     return (
         <>
         
             <Link className={style.link} href={page} onClick={handleClick}>
-                <div className={style.button}>{title}</div>
+                <div className={style.button} style={{color:fontColorMap[context.state.page]}}>
+                    {title}
+                </div>
             </Link>
 
         </>
