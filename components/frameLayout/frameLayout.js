3787f3fa2040c2b1eb4ea28615a48f95d682237c
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-04-11 23:46:23 -0300

    @frameLayout: context-dependent background color

--- components/frameLayout/frameLayout.js
+++ components/frameLayout/frameLayout.js
@@ -1,13 +1,23 @@
 import Cube from "../cube/cube";
 import NavButtonCollection from "../navButton/NavButtonGroup/NavButtonGroup";
 import style from "./frameLayout.module.scss"
+import { useContext } from "react";
+import AppContext from "@/context/AppContext";
 
 const FrameLayout = ({children}) => {
+    const context = useContext(AppContext)
+    const backgroundColorMap = {
+        "Home":         "#F4EEE0",
+        "Projects":     "#F4EEE0",
+        "Blog":         "#F4EEE0",
+        "CV":           "#F4EEE0",
+        "Technologies": "#F4EEE0",
+        "Contact":      "#F4EEE0",
+    }
     return (
         <>
         
-        <div className={style.layoutContainer}>
-
+        <div className={style.layoutContainer} style={{backgroundColor:backgroundColorMap[context.state.page]}}>
             <div className={style.buttonsContainer}>
                 <NavButtonCollection/>
             </div>
@@ -17,13 +27,12 @@ const FrameLayout = ({children}) => {
                 <div className={style.cube}>
                     <Cube/>
                 </div>
-
                 <div className={style.childrenContainer}>
                     {children}
                 </div>
             </div>
-        </div>
 
+        </div>
 
         </>
     );
