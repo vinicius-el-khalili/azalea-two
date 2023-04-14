d32740920a4acb7b79f53b4fb28c10a16477bd7a
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-04-13 22:22:48 -0300

    neon css done; neon html done

--- pages/technologies/index.js
+++ pages/technologies/index.js
@@ -1,21 +1,23 @@
 import Image from "next/image";
+import { useEffect, useContext } from "react";
+import AppContext from "@/context/AppContext";
+
 import style from "./technologies.module.scss"
 
 import NeonCSS from "./neonIcons/neonCSS";
 import NeonSVG from "./neonIcons/neonSVG";
 
 const Technologies = () => {
+    const context = useContext(AppContext)
+    useEffect(()=>{
+        context.setPage("Technologies")
+    },[])
     return (
         <>
         
         <div className={style.layout}>
-            <Image 
-            src={"/technologies/html.svg"}
-            width={350}
-            height={350}
-            />
-            <NeonCSS size={150}/>
             <NeonSVG target={"css"} shadowColor={"#33a9ec"}/>
+            <NeonSVG target={"html"} shadowColor={"#F16529"}/>
         </div>
         
         </>
