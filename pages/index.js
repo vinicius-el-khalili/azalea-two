8b7f09da2f4dabb2ec48c4063db5ff60cc2db054
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-04-17 23:12:45 -0300

    quick fix

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
           
           <TypedWords
           texts={[
-            `< >< / >`,
+            `<></>`,
             `{ ' ' : { } }`,
             `[ ... { } ]`,
             `? { } : { } `
